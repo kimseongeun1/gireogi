60dbad894a28deaeabe46c9ed16427cfeea74ce3
kimseongeun1 <sg0294310@gmail.com>
2022-02-20 22:44:06 +0900

    공모전 페이지를 만들었다

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='one'>
+        <div className='back'>←</div>
+        <div className='maintitle'><strong>공모전 게시글 제목</strong></div>
+      </div>
+
+      <div className='two'>
+        <div className='left'>
+          <div className='picture'></div>
+          <div className='go'>팀원 모집하러 가기</div>
+        </div>
+        <div className='right'>
+          <div className='title'>
+            <div className='title2'><strong>공모전 게시글 제목</strong></div>
+            <div className='time'><strong>♕접수기간 : 2022/01/01 ~ 2022/02/01</strong></div>
+          </div>
+          <div className='contents'>
+            <ul>
+              <li>기업 형태 : 중소기업/스타트업</li>
+              <li>참여 대상 : 대상 제한 없음</li>
+              <li>시상 규모 : 100만원</li>
+              <li>활동 혜택 : 실제 상용화</li>
+              <li>홈페이지 : -</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className='three'>저 오늘 첵스초코 우유에 말아먹었어요 저 영어 문장 분명 나오게 하는 방법이 있었던 것 같은데 뭐더라 기억이 안 난다</div>
     </div>
   );
 }
